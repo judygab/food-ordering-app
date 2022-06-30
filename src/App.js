4912e8f57ad8ba4704bb8d76278e9d27f4cecc78
judygab <ivditigabeskiria@gmail.com>
2022-06-30 18:44:43 -0400

    added header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import { Header } from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+export const Header = () => {
+    return (
+        <nav id="header" className="fixed w-full z-30 top-0 text-white">
+            <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
+                <div className="pl-4 flex items-center">
+                    <a className="toggleColor text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl" href="/">
+                        {/* Icon goes here */}
+                        LOGO
+                    </a>
+                </div>
+                <div class="block lg:hidden pr-4">
+                    <button id="nav-toggle" class="flex items-center p-1 text-pink-800 hover:text-gray-900 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+                        <svg class="fill-current h-6 w-6" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <title>Menu</title>
+                            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+                        </svg>
+                    </button>
+                </div>
+            </div>
+        </nav>
+    )
+};
