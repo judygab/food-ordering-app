45c6c3978fbacc9dbe054c4bb02c9efea5f6cf6f
vaniillajs <vaniillajs@gmail.com>
2022-07-06 17:21:24 +0530

    feat: fonts udpated, reusable components are added

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,51 +1,53 @@
 import { useForm } from 'react-hook-form';
+import Button from '../../components/elements/Button';
+import Text from '../../components/elements/Text';
 
 const Login = () => {
   const { register, handleSubmit } = useForm();
   const onSubmit = (data) => console.log(data);
 
   return (
-    <div className="h-screen flex  items-center justify-center bg-black">
-      <div className=" p-10 rounded-lg max-w-md w-full flex flex-col items-center justify-center border-4">
-        <p className="text-5xl text-white">Login</p>
-        <div className="h-5"></div>
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className="w-full space-y-6  rounded-lg"
-        >
-          <div className="">
-            <label
-              htmlFor="email"
-              className="block text-lg font-medium text-gray-200 "
-            >
-              Email address
-            </label>
-            <input
-              {...register('email')}
-              id="email"
-              type={'email'}
-              className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-daintree focus:border-daintree sm:text-lg"
-            />
-          </div>
-          <div className="">
-            <label
-              htmlFor="email"
-              className="block text-lg font-medium text-gray-200 "
-            >
-              Password
-            </label>
-            <input
-              {...register('password')}
-              id="password"
-              type={'password'}
-              className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-gray-200 focus:border-gray-100 sm:text-lg"
-            />
-          </div>
-          <div className="h-2"></div>
-          <button className="w-full py-2 rounded-lg cursor-pointer bg-red-700 hover:bg-red-800 text-white">
-            Login
-          </button>
-        </form>
+    <div className="h-screen flex items-center justify-center bg-black">
+      <div className="rounded-lg max-w-md w-full flex flex-col items-center justify-center relative">
+        {/* absolute div with gradient in the backend will act as the border */}
+        <div className="absolute inset-0 transition duration-300 animate-pink blur  gradient bg-gradient-to-tr from-rose-500 to-yellow-500"></div>
+        <div className="p-10 rounded-xl z-10 bg-black w-full h-full flex flex-col items-center justify-center">
+          <Text variant="hero">Login</Text>
+          <div className="h-10"></div>
+          <form onSubmit={handleSubmit(onSubmit)} className="w-full space-y-6">
+            <div className="">
+              <label
+                htmlFor="email"
+                className="block text-lg font-medium text-gray-200 "
+              >
+                Email
+              </label>
+              <input
+                {...register('email')}
+                id="email"
+                type={'email'}
+                className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-gray-200 focus:border-gray-200"
+              />
+            </div>
+            <div className="">
+              <label
+                htmlFor="email"
+                className="block  font-medium text-gray-200 "
+              >
+                Password
+              </label>
+              <input
+                {...register('password')}
+                id="password"
+                type={'password'}
+                className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-gray-200 focus:border-gray-100"
+              />
+            </div>
+            <div className="h-2"></div>
+
+            <Button size="large">Sign In</Button>
+          </form>
+        </div>
       </div>
     </div>
   );
--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,4 +1,6 @@
 import { useForm } from 'react-hook-form';
+import Button from '../../components/elements/Button';
+import Text from '../../components/elements/Text';
 
 const Register = () => {
   const { register, handleSubmit } = useForm();
@@ -6,61 +8,59 @@ const Register = () => {
 
   return (
     <div className="h-screen bg-black flex  items-center justify-center">
-      <div className="border-4 p-10 rounded-lg max-w-md w-full flex flex-col items-center justify-center">
-        <p className="text-5xl text-white">Register</p>
-        <div className="h-5"></div>
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className="w-full space-y-6  rounded-lg"
-        >
-          <div className="">
-            <label
-              htmlFor="email"
-              className="block text-lg font-medium text-gray-200 "
-            >
-              Name
-            </label>
-            <input
-              {...register('name')}
-              id="name"
-              type={'text'}
-              className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-daintree focus:border-daintree sm:text-lg"
-            />
-          </div>
-          <div className="">
-            <label
-              htmlFor="email"
-              className="block text-lg font-medium text-gray-200 "
-            >
-              Email address
-            </label>
-            <input
-              {...register('email')}
-              id="email"
-              type={'email'}
-              className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-daintree focus:border-daintree sm:text-lg"
-            />
-          </div>
-          <div className="">
-            <label
-              htmlFor="email"
-              className="block text-lg font-medium text-gray-200 "
-            >
-              Password
-            </label>
-            <input
-              {...register('password')}
-              id="password"
-              type={'password'}
-              className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-gray-200 focus:border-gray-100 sm:text-lg"
-            />
-          </div>
-          <div className="h-2"></div>
-          <input
-            type="submit"
-            className="bg-white w-full py-2 rounded-lg cursor-pointer"
-          />
-        </form>
+      <div className="rounded-lg max-w-md w-full flex flex-col items-center justify-center relative">
+        {/* absolute div with gradient in the backend will act as the border */}
+        <div className="absolute inset-0 transition duration-300 animate-pink blur  gradient bg-gradient-to-tr from-rose-500 to-yellow-500"></div>
+        <div className="p-10 rounded-xl z-10 w-full h-full bg-black">
+          <Text variant="hero">Register</Text>
+          <div className="h-5"></div>
+          <form onSubmit={handleSubmit(onSubmit)} className="w-full space-y-6">
+            <div className="">
+              <label
+                htmlFor="email"
+                className="block text-lg font-medium text-gray-200 "
+              >
+                Name
+              </label>
+              <input
+                {...register('name')}
+                id="name"
+                type={'text'}
+                className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-gray-200 focus:border-gray-200"
+              />
+            </div>
+            <div className="">
+              <label
+                htmlFor="email"
+                className="block text-lg font-medium text-gray-200 "
+              >
+                Email address
+              </label>
+              <input
+                {...register('email')}
+                id="email"
+                type={'email'}
+                className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-gray-200 focus:border-gray-200"
+              />
+            </div>
+            <div className="">
+              <label
+                htmlFor="email"
+                className="block text-lg font-medium text-gray-200 "
+              >
+                Password
+              </label>
+              <input
+                {...register('password')}
+                id="password"
+                type={'password'}
+                className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-gray-200 focus:border-gray-100"
+              />
+            </div>
+            <div className="h-2"></div>
+            <Button size="large">Register</Button>
+          </form>
+        </div>
       </div>
     </div>
   );
