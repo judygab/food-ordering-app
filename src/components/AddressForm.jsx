a66aaad504fdb10819b58a866cdd07d7f4c3b7ec
judygab <ivditigabeskiria@gmail.com>
2022-07-08 21:49:02 -0400

    updated form styling

--- src/components/AddressForm.jsx
+++ src/components/AddressForm.jsx
@@ -3,8 +3,8 @@ import { useForm } from 'react-hook-form';
 export const AddressForm = () => {
     const onSubmit = (data) => console.log(data);
     return (
-        <form>
-        <h3 className="pt-4 text-2xl text-center">Address for the delivery</h3>
+        <form className="md:w-2/3 md:mx-auto px-2 pt-1">
+        <h3 className="pt-4 text-2xl md:text-center">Address for the delivery</h3>
         <div className="mb-4">
             <label class="block mb-2 text-sm font-bold text-gray-700" for="streetAddress">
                 Street Address
@@ -17,7 +17,7 @@ export const AddressForm = () => {
             />
         </div>
         <div className="mb-4 md:flex md:justify-between">
-            <div className="mb-4 md:mr-2 md:mb-0">
+            <div className="mb-4 md:mr-2 md:mb-0 flex-1">
                 <label className="block mb-2 text-sm font-bold text-gray-700" for="city">City</label>
                 <input
                     class="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
@@ -26,7 +26,7 @@ export const AddressForm = () => {
                     placeholder="City"
                 />
             </div>
-            <div className="mb-4 md:mr-2 md:mb-0">
+            <div className="mb-4 md:mr-2 md:mb-0 flex-1">
                 <label className="block mb-2 text-sm font-bold text-gray-700" for="state">State</label>
                 <input
                     class="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
--- src/components/PaymentForm.jsx
+++ src/components/PaymentForm.jsx
@@ -3,10 +3,10 @@ import { useForm } from 'react-hook-form';
 export const PaymentForm = () => {
     const onSubmit = (data) => console.log(data);
     return (
-        <form>
-            <h3 className="pt-4 text-2xl text-center">Please enter your card details</h3>
+        <form className="md:w-2/3 md:mx-auto px-2 pt-1">
+            <h3 className="pt-4 text-2xl md:text-center">Please enter your card details</h3>
             <div className="mb-4 md:flex md:justify-between">
-                <div className="mb-4 md:mr-2 md:mb-0">
+                <div className="mb-4 md:mr-2 md:mb-0 flex-1">
                     <label className="block mb-2 text-sm font-bold text-gray-700" for="firstName">First Name</label>
                     <input
                         class="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
@@ -15,7 +15,7 @@ export const PaymentForm = () => {
                         placeholder="First Name"
                     />
                 </div>
-                <div className="mb-4 md:mr-2 md:mb-0">
+                <div className="mb-4 md:mr-2 md:mb-0 flex-1">
                     <label className="block mb-2 text-sm font-bold text-gray-700" for="lastName">Last Name</label>
                     <input
                         class="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
